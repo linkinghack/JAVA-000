784d5b6621aa1bf2fd2a6a310117a2c2dedc5cdc
linkinghack <linkinghack@outlook.com>
2020-11-11 09:57:56 +0800

    Add Week1 home work

---
+++ Week_01/code/HelloClassLoader.java
@@ -0,0 +1,46 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+
+/**
+ * HelloClassLoader
+ */
+public class HelloClassLoader extends ClassLoader {
+
+    private static String helloClassFileName = "./Hello.xlass";
+
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            helloClassFileName = args[0];
+        }
+
+        try {
+            
+            HelloClassLoader classLoader = new HelloClassLoader();
+            byte[] classBytes = classLoader.loadClassFile(helloClassFileName);
+            Class<?> helloClass = classLoader.parseClassFromBytes("Hello", classBytes);
+            Object helloObj = helloClass.getConstructor().newInstance();
+            helloClass.getMethod("hello").invoke(helloObj);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Class<?> parseClassFromBytes(String name, byte[] classBytes) {
+        return defineClass(name, classBytes, 0, classBytes.length);
+    }
+
+    private byte[] loadClassFile(String fileName) throws FileNotFoundException, IOException {
+        InputStream is = new FileInputStream(fileName);
+        byte[] fileBytes = is.readAllBytes();
+        for (int i = 0; i < fileBytes.length; i++) {
+            fileBytes[i] = (byte) (255 - fileBytes[i]);
+        }
+        is.close();
+        return fileBytes;
+    }
+
+}
